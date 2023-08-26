d2e6976a4bbdc0bfeb2358f0d3f167329520b17b
RobertJohnsonSequencerPro <Robert.Johnson@SequencerPro.com>
2023-08-26 15:58:32 -0600

    Add project files.

---
+++ clsCollector.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plain_Old_Class_Object__POCO__Pattern
+{
+    internal class clsCollector
+    {
+
+        string strCollectorType = "";
+        string strDescription = "";
+        //The collector ID is the primary key for the database. It is always a positive integer value. Assigned here intitally as -1 to indicate that it has not been correctly initialized with data from the database.
+        long lngCollectorID = -1;
+
+
+        public enum CollectorFields
+        {
+            strCollectorType,
+            strDescription,
+            lngCollectorID
+        }
+
+        public enum CollectorTypes
+        {
+            Text,
+            Numeric,
+            Category,
+            Linked,
+            PassFail
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dr">dr must contain columns as given in the Enum CollectorFields</param>
+        public clsCollector(DataRow dr)
+        {
+            strCollectorType = dr[CollectorFields.strCollectorType.ToString()].ToString();
+            strDescription = dr[CollectorFields.strDescription.ToString()].ToString();
+            long.TryParse(dr[CollectorFields.lngCollectorID.ToString()].ToString(), out long lngCollector);
+        }
+
+
+
+
+    }
+}
---
+++ clsStep.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plain_Old_Class_Object__POCO__Pattern
+{
+    public class clsStep
+    {
+
+        public enum StepFields
+        {
+            lngStepID
+        }
+
+        List<clsCollector> Collectors = new List<clsCollector>();
+
+        //The primary key for the step record in the database. It is always a positive integer. Set here to -1 to indicate that it has not yet been initialized with valid data.
+        long lngStepID = -1;
+
+
+
+        public clsStep(DataRow dr)
+        {
+
+           long.TryParse(dr[StepFields.lngStepID.ToString()].ToString(), out long lngStepID);
+
+            string query = "SELECT * FROM Collector WHERE " + StepFields.lngStepID.ToString() + " = '" + lngStepID +"'";
+            System.Data.DataTable dt = new System.Data.DataTable();
+            //Write some code that executes the above query and stffs the result into  
+
+            foreach(DataRow drNewCollector in dt.Rows )
+            {
+                clsCollector newCollector = new clsCollector(drNewCollector);
+                Collectors.Add(newCollector);
+            }
+        }
+
+    }
+}
