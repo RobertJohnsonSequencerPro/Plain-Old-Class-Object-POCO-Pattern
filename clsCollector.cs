9b240c25f8f798825cde17f0b9b3376e843df1c0
RobertJohnsonSequencerPro <Robert.Johnson@SequencerPro.com>
2023-08-27 16:14:25 -0600

    Added some documentation to improve readability of this example code.

--- clsCollector.cs
+++ clsCollector.cs
@@ -15,7 +15,9 @@ namespace Plain_Old_Class_Object__POCO__Pattern
         //The collector ID is the primary key for the database. It is always a positive integer value. Assigned here intitally as -1 to indicate that it has not been correctly initialized with data from the database.
         long lngCollectorID = -1;
 
-
+        /// <summary>
+        /// Listing of the fields in this class -- should match the fields in the Step table of the database.
+        /// </summary>
         public enum CollectorFields
         {
             strCollectorType,
@@ -23,6 +25,9 @@ namespace Plain_Old_Class_Object__POCO__Pattern
             lngCollectorID
         }
 
+        /// <summary>
+        /// Listing of the possible collector types.
+        /// </summary>
         public enum CollectorTypes
         {
             Text,
@@ -33,11 +38,12 @@ namespace Plain_Old_Class_Object__POCO__Pattern
         }
 
         /// <summary>
-        /// 
+        /// Get a datarow from the Collector table in the database and pass into this constructor to instantiate a Collector object.
         /// </summary>
         /// <param name="dr">dr must contain columns as given in the Enum CollectorFields</param>
         public clsCollector(DataRow dr)
         {
+            //Parse the type, description, and ID from the datarow that was passed into the constructor and use to set those fields in this object.
             strCollectorType = dr[CollectorFields.strCollectorType.ToString()].ToString();
             strDescription = dr[CollectorFields.strDescription.ToString()].ToString();
             long.TryParse(dr[CollectorFields.lngCollectorID.ToString()].ToString(), out long lngCollector);
--- clsStep.cs
+++ clsStep.cs
@@ -10,29 +10,40 @@ namespace Plain_Old_Class_Object__POCO__Pattern
 {
     public class clsStep
     {
-
+        /// <summary>
+        /// Listing of the fields in this class -- should match the fields in the Step table of the database.
+        /// </summary>
         public enum StepFields
         {
             lngStepID
         }
 
+        /// <summary>
+        /// List of collector objects that belong to an instance of this class.
+        /// </summary>
         List<clsCollector> Collectors = new List<clsCollector>();
 
-        //The primary key for the step record in the database. It is always a positive integer. Set here to -1 to indicate that it has not yet been initialized with valid data.
+        /// <summary>
+        /// The primary key for the step record in the database. It is always a positive integer. Set here to -1 to indicate that it has not yet been initialized with valid data.
+        /// </summary>
         long lngStepID = -1;
 
 
-
+        /// <summary>
+        /// Constructor for the step object. Get datarow from the Step table in the db and pass into this constructor.
+        /// </summary>
+        /// <param name="dr">The datarow dr must have fields as listed in the StepFileds enum.</param>
         public clsStep(DataRow dr)
         {
-
+            //Parse the Step ID (long) from the datarow that was passed into the constructor.
            long.TryParse(dr[StepFields.lngStepID.ToString()].ToString(), out long lngStepID);
 
-           
+            //Use the Step ID to find collecotrs that belong to this step.
             System.Data.DataTable dt = new System.Data.DataTable();
             clsSQLiteConn newSQLiteConn = new clsSQLiteConn();
             newSQLiteConn.PopulateADataTable(ref dt, "Collector");
 
+            //Instantiate a list of collectors that belong to this step.
             foreach(DataRow drNewCollector in dt.Rows )
             {
                 clsCollector newCollector = new clsCollector(drNewCollector);
